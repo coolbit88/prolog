8f0bd8c69a9a48d5f2a7514ae98c0e3c913aa30e
coolbit88 <collaboration@yeah.net>
2022-05-25 20:27:06 +0800

    second

---
+++ internal/server/log.go
@@ -0,0 +1,22 @@
+package server
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Log struct {
+	mu      sync.Mutex
+	records []Record
+}
+
+func NewLog() *Log {
+	return &Log{}
+}
+
+type Record struct {
+	Value  []byte `json:"value"`
+	Offset uint64 `json:"offset"`
+}
+
+var ErrOffsetNotFound = fmt.Errorf("offset not found")
